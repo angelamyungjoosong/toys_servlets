ea2dc887e0856b6dde063292a3a5a00554460297
angelamyungjoosong <angelaforever122@gmail.com>
2023-06-26 18:45:42 +0900

    first commit

---
+++ src/main/java/com/example/toys_servlets/ToysServletsApplication.java
@@ -0,0 +1,13 @@
+package com.example.toys_servlets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysServletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToysServletsApplication.class, args);
+	}
+
+}
