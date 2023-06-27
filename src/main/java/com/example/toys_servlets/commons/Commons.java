e309a8f19c9228c64f41e4be275ffc829c78bd17
angelamyungjoosong <angelaforever122@gmail.com>
2023-06-27 11:14:40 +0900

    6.27

--- src/main/java/com/example/toys_servlets/commons/Commons.java
+++ src/main/java/com/example/toys_servlets/commons/Commons.java
@@ -1,4 +1,4 @@
-package com.example.study_servlet.commons;
+package com.example.toys_servlets.commons;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
---
+++ src/main/java/com/example/toys_servlets/controls/Statistic.java
@@ -0,0 +1,5 @@
+package com.example.toys_servlets.controls;
+
+public class Statistic {
+    
+}
