cbf138637ae76bf2429b2a232f2beeaf7f583948
jeongjiwooon <jjwanm@naver.com>
2023-06-28 08:29:49 +0900

    commit

--- src/main/java/com/example/toys_servlets/daos/PollDao.java
+++ src/main/java/com/example/toys_servlets/daos/PollDao.java
@@ -26,7 +26,7 @@ public class PollDao {
             System.out.println(e.getMessage());
         }
         return cnt;
-}
+    }
 
     // 회원 리스트 메소드
     public ArrayList selectAll(String unique_id) {
@@ -48,6 +48,50 @@ public class PollDao {
             System.out.println(e.getMessage());
         }
 
+        return arrayList;
+    }
+
+    public ArrayList Survey()
+    {
+        int number = 1;
+        int second_number = 1;
+        ArrayList arrayList = new ArrayList();
+        try
+        {
+            Commons commons = new Commons();
+            Statement statement = commons.getStatement();
+            String query = "select *\n" + //
+                    "from question_choice;";
+            ResultSet resultSet = statement.executeQuery(query);
+            HashMap<String, String> questionInfor = new HashMap<>();
+            HashMap<String, String> choiceInfor = new HashMap<>();
+            while (resultSet.next())
+            {
+                query = "select cho.choice_id, cho.choice\n" + //
+                        "from question_choice as ques_cho\n" + //
+                        "inner join choice as cho\n" + //
+                        "on ques_cho.choice_id = cho.choice_id\n" + //
+                        "and questions_id = 'Q-01';";
+                // second_number = second_number + 1;
+                ResultSet resultSet_second = statement.executeQuery(query);
+                while (resultSet_second.next())
+                {
+                    // questionInfor.put();
+                    if (number < 5)
+                    {
+                        choiceInfor.put(String.valueOf(number), resultSet_second.getString("choice_id"));
+                        number = number + 1;
+                    }
+                }
+                // questionInfor.put()
+                second_number = second_number + 1;
+            }
+        }
+
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
         return arrayList;
     }
 }
