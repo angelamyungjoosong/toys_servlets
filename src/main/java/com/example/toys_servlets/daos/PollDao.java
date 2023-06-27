e8058cc441fa7065413decf64aaadb3e4af0b7ac
estskyway <estskyway@gmail.com>
2023-06-27 20:05:40 +0900

    6/27 20시 5분 임거산

--- src/main/java/com/example/toys_servlets/daos/PollDao.java
+++ src/main/java/com/example/toys_servlets/daos/PollDao.java
@@ -11,7 +11,7 @@ import com.example.toys_servlets.commons.Commons;
 public class PollDao{
     //통계 참여자 총수 메소드
     public int PollDaoCountServlet(){
-      
+        int cnt = 0;
         try {
             Commons commons = new Commons();
             Statement statement = commons.getStatement();
@@ -22,7 +22,7 @@ public class PollDao{
                     "";
                     
            ResultSet resultSet = statement.executeQuery(query); 
-           int cnt = resultSet.getInt(query);
+           cnt = resultSet.getInt(query);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -51,4 +51,27 @@ public class PollDao{
 
         return arrayList;
     }
+
+    // 회원상세정보 메소드
+    public ArrayList Password(String password) {
+        ArrayList arrayList = new ArrayList();
+        try {
+            Commons commons = new Commons();
+            Statement statement = commons.getStatement();
+            String query = "SELECT * \n" + //
+                    "FROM db_survey_project.respondents;";
+            ResultSet resultSet = statement.executeQuery(query);// resultset은 재활용 가능
+            HashMap hashMap = new HashMap();
+                hashMap = new HashMap();
+                hashMap.put("RESPONDENTS", resultSet.getString("RESPONDENTS"));
+                hashMap.put("PASSWORDS", resultSet.getString("PASSWORDS"));
+                arrayList.add(hashMap);
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return arrayList;
+    }
+
 }
